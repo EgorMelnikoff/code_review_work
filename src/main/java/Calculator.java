bde887dc53694b11235105c4c23d72cd5c857144
xineigor <triazeya@gmail.com>
2025-12-02 17:01:20 +0300

    fix /0 by Lyalin

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -8,6 +8,7 @@ public class Calculator {
     }
 
     public int div(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("Делить на ноль нельзя!");
         return a / b;
     }
 
