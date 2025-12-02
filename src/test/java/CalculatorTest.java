373e2f8ada1508b9be536a01ce76aa572f96ce31
xineigor <triazeya@gmail.com>
2025-12-02 14:59:04 +0300

    Implemented tests by Lyalin

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,28 +4,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    private final Calculator calculator = new Calculator();
+
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        assertEquals(5, calculator.add(2, 3));
+        assertEquals(0, calculator.add(-2, 2));
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        assertEquals(3, calculator.dif(5, 2));
+        assertEquals(-5, calculator.dif(5, 10));
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        assertEquals(5, calculator.div(10, 2));
+        assertEquals(2, calculator.div(5, 2));
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        assertEquals(6, calculator.times(2, 3));
+        assertEquals(0, calculator.times(100, 0));
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        assertEquals(30, calculator.solver());
     }
 }
