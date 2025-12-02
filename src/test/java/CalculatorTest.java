cf31f2a5141e72091e6d39261e00d7674b4a5b77
egor <goodperson2033@gmail.com>
2025-12-02 14:56:24 +0300

    Implemented solver method, updated readme by Melnikov

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,29 +3,30 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    Calculator calculator = new Calculator();
 
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        assertEquals(2, calculator.add(1, 1));
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        assertEquals(4, calculator.dif(16, 12));
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        assertEquals(4, calculator.div(16, 4));
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        assertEquals(25, calculator.times(5, 5));
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        assertEquals(5, calculator.solver(5, 5, 5));
     }
 }
